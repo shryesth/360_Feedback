121c2cc2df54dd0f7514bf60f7c86039cb05b50c
Shryesth Pandey <shryesth4236@gmail.com>
2023-12-11 07:03:58 +0530

    downloading epapers

--- Modules/epaper_scraper/paper_scraper.py
+++ Modules/epaper_scraper/paper_scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import os.path
 from datetime import date, timedelta
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -24,9 +25,18 @@ def get_newspaper_links(url):
     soup = BeautifulSoup(response.content, 'html.parser')
     return [tag.get('href', '') for tag in soup('a') if 'https://vk.com/' in tag.get('href', '')]
 
-def download_pdf_with_button_click(link, filename):
-    # Initialize a browser (you may need to download a suitable driver)
-    driver = webdriver.Chrome()
+
+def download_pdf_with_button_click(link, filename, target_directory):
+    # Set download preferences to force external download
+    chrome_options = webdriver.ChromeOptions()
+    prefs = {
+        "plugins.always_open_pdf_externally": True,
+        "download.default_directory": target_directory,  # Set the download directory
+    }
+    chrome_options.add_experimental_option("prefs", prefs)
+
+    # Initialize a browser with the specified options
+    driver = webdriver.Chrome(options=chrome_options)
 
     try:
         # Open the link
@@ -38,20 +48,44 @@ def download_pdf_with_button_click(link, filename):
             EC.presence_of_element_located((By.XPATH, button_xpath))
         )
 
+        # Record the start time for download
+        start_time = time.time()
+
         # Click the button
         download_button.click()
 
-        # Wait for the download to complete (adjust the sleep time as needed)
-        time.sleep(5)
+        # Poll for the completion of the download
+        max_poll_attempts = 60  # Maximum attempts to check for the file
+        poll_interval = 1  # Time interval between polling attempts
 
-        # Rename the downloaded file (adjust the filename accordingly)
-        os.rename("original_filename.pdf", filename)
-        print(f'Done downloading: {filename}')
+        for _ in range(max_poll_attempts):
+            files = [file for file in os.listdir(target_directory) if file.endswith((".pdf", ".crdownload"))]
+            if any(file.endswith(".pdf") for file in files):
+                # Get the name of the downloaded file
+                downloaded_file = [file for file in files if file.endswith(".pdf")][0]
+                # Rename the downloaded file
+                os.rename(os.path.join(target_directory, downloaded_file), os.path.join(target_directory, filename))
+                print(f'Done downloading: {filename}')
+                break
+            elif any(file.endswith(".crdownload") for file in files):
+                print("Download in progress. Waiting for completion...")
+            else:
+                print("No download file detected.")
+            time.sleep(poll_interval)
+        else:
+            print(f'Timeout: Download may not have completed in time.')
+
+        # Record the end time for download
+        end_time = time.time()
+        print(f'Time taken for download: {end_time - start_time} seconds')
+
+    except Exception as e:
+        print(f'Error during download: {e}')
 
     finally:
         # Close the browser
         driver.quit()
-
+        
 def main():
     today = date.today()
 
@@ -68,16 +102,12 @@ def main():
             newspaper_detail = NEWSPAPERS[newspaper_number - 1]
             links = get_newspaper_links(newspaper_detail['url'])
 
-            directory = newspaper_detail['name']
-            parent_dir = os.getcwd()
-            path = os.path.join(parent_dir, directory)
-
+            target_directory = os.path.join(os.getcwd(), newspaper_detail['name'])
             try:
-                os.mkdir(path)
+                os.mkdir(target_directory)
             except OSError:
                 pass
 
-            os.chdir(path)  # Context manager for changing directory
             for i in range(for_how_many_days):
                 if i < len(links):
                     url = links[i]
@@ -85,13 +115,12 @@ def main():
 
                     if is_downloadable(url):
                         print(f'Downloading {newspaper_detail["name"]} for {date_that_day}...')
-                        download_pdf_with_button_click(url, f"{newspaper_detail['name']}_{date_that_day}.pdf")
+                        download_pdf_with_button_click(url, f"{newspaper_detail['name']}_{date_that_day}.pdf", target_directory)
                         print('Done :)')
                     else:
                         print(f'{newspaper_detail["name"]} paper not available for {date_that_day}')
                 else:
                     print(f'No link available for {newspaper_detail["name"]} on day {i + 1}')
-            os.chdir('../')  # Return to the parent directory
 
         else:
             print(f"Invalid newspaper number: {newspaper_number}")
